5ab4638f1de01939860846220d67ee0835684e0a
Lúcio Couto <lucio.couto@sinqia.com.br>
2020-08-10 14:33:28 -0300

    Primeiro commit

---
+++ AnoBisexto/AnoUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnoBisexto
+{
+    public static class AnoUtil
+    {
+        public  static bool VerificarseAnoEhBisexto(int aAno)
+        {
+            bool anoEhBisexto = false;
+
+            anoEhBisexto = (((aAno % 4) == 0) && ((aAno % 100) != 0) || ((aAno % 400) == 0));
+
+            return anoEhBisexto;
+        }
+    }
+}
---
+++ AnoBisexto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnoBisexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado;
+
+            var ano = int.Parse(args[0]);
+
+            bool bisexto = AnoUtil.VerificarseAnoEhBisexto(ano);
+
+            string mensagemResultado = bisexto ? " é bisexto." : " não é bisexto.";
+
+            string mensagem = $"o Ano {args[0]} {mensagemResultado} ";
+
+            Console.WriteLine(mensagem);
+            Console.ReadKey();
+        }       
+    }
+}
